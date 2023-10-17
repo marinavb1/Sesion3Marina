4b8c77241fdc77cca7e82c74e8bc4971a8dac7d9
marin <marin@Marina>
2023-10-17 20:00:33 +0200

    TDD primer paso, crear prueba (insertar)

---
+++ src/pkg/Cuenta.java
@@ -0,0 +1,9 @@
+package pkg;
+
+public class Cuenta {
+
+	public Cuenta() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/test/CuentaTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CuentaTest {
+	Cuenta ctaPruebas;
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		ctaPruebas=new Cuenta();
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testIngresar() {
+		ctaPrueba.ingresar(3000);
+		assertEquals(3000, ctaPruebas.getSaldo());
+	}
+	
+
+}
